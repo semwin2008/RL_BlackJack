844f1c4c2b5edbf286aedd201be108640ff2031c
semwin2008 <semwin2008@gmail.com>
2026-09-18 13:51:32 +0300

    added agent logic

--- agent.py
+++ agent.py
@@ -1,6 +1,15 @@
-class Agent():
+import torch
+import torch.nn as nn
+
+class Agent(nn.Module):
     def __init__(self, **params):
-        pass
+        super().__init__()
+
+        self.network = nn.Linear(1, 2)
+        self.activ = nn.Sigmoid()
+
+    def forward(self, x):
+        return self.activ(self.network(x))
 
     def act(self, state):
-        pass
+        return self.forward(state)
